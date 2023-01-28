7f99be975b8d24a368a1cb6ac37c1d9d306bb964
Nima <50780747+JacksonMelloGang@users.noreply.github.com>
2023-01-28 23:55:34 +0100

    almost done

--- FirefoxExtension/index.js
+++ FirefoxExtension/index.js
@@ -4,38 +4,50 @@ var getInfos = function() {
     // get concerned elements
     var title_element = document.getElementsByClassName("title style-scope ytmusic-player-bar"); 
     var artist_element = document.getElementsByClassName("byline style-scope ytmusic-player-bar complex-string");
-    var progress_bar = document.getElementsByTagName("tp-yt-paper-slider")[2];
-    var time = document.getElementsByClassName("time-info style-scope ytmusic-player-bar")[0].innerText.toString().split(' / ')[1];
+    //var time = document.querySelector('span.time-info[dir="ltr"].style-scope.ytmusic-player-bar');
+    var time = document.getElementsByClassName('time-info style-scope ytmusic-player-bar')[0];
 
     var infos = {
         // get infos from youtube music page
         title: title_element.length != 0 ? title_element[0].textContent : "undefined",
         artist: artist_element.length != 0 ? artist_element[0].textContent.split("•")[0] : "undefined",
-        time: time,
-        //playedtime: progress_bar.value,
-        //maxtime: progress_bar.ariaValueMax
-        //time: progress_bar.ariaValueText
+        time: time.innerHTML.trim().split(" / ")[0],
+        max_time: time.innerHTML.trim().split(" / ")[1]
     };
 
     return infos;
 };
 
-console.log("Content script loaded");
+// handle events from the background script
+var handleEvent = function(request, sender, sendResponse) {
+    console.log("Received message from background script");
 
-// when getting a message from the background script, execute getInfos function
-// and send the result to the background script
-browser.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        console.log("Received message from background script");
+    browser.runtime.sendMessage({
+        infos: getInfos()
+    });
 
-        // send infos to the background script
+};
+
+browser.runtime.onMessage.addListener(handleEvent);
+
+let title = getInfos().title;
+let author = getInfos().artist;
+let max_time = getInfos().max_time;
+
+var intervalId = setInterval(function(){
+    //console.log(`Saved title: ${title} | Saved author: ${author} | Saved max time: ${max_time}`);
+    console.log(`Title: ${getInfos().title} | Author: ${getInfos().artist} | Time: ${getInfos().time} | Max Time: ${getInfos().max_time}`);
+
+    if(title != getInfos().title || author != getInfos().artist || max_time != getInfos().max_time){
+
+        title = getInfos().title;
+        author = getInfos().artist;
+        max_time = getInfos().max_time;
+
+        setTimeout(() => {}, 3000);
+
         browser.runtime.sendMessage({
             infos: getInfos()
         });
-
-        sendResponse({response: "Message Received! (content)"});
-        return true;
-    });
-
-
-
+    }
+}, 1000)
